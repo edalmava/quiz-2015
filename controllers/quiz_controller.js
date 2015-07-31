477b0b4711d881fd1d4984a4f9a0cb7beb0cf698
edalmava <eamartinezv@gmail.com>
2015-07-31 12:21:45 -0500

    Modelo y BD

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,11 +1,23 @@
+var models = require('../models/models.js');
+
 exports.question = function(req, res) {
-	res.render('quizes/question', {pregunta: 'Capital de Italia', title: 'Quiz'});
+	//res.render('quizes/question', {pregunta: 'Capital de Italia', title: 'Quiz'});
+	models.Quiz.findAll().then(function(quiz) {
+		res.render('quizes/question', { pregunta: quiz[0].pregunta, title: 'Quiz' });
+    })
 };
 
 exports.answer = function(req, res) {
-	if (req.query.respuesta === 'Roma'){
+	/*if (req.query.respuesta === 'Roma'){
 		res.render('quizes/answer', {respuesta: 'Correcto', title: 'Quiz'});
 	} else {
 		res.render('quizes/answer', {respuesta: 'Incorrecto', title: 'Quiz'});
-	}
+	}*/
+	models.Quiz.findAll().then(function(quiz) {
+		if (req.query.respuesta === quiz[0].respuesta) {
+			res.render('quizes/answer', { respuesta: 'Correcto', title: 'Quiz' });
+		} else {
+			res.render('quizes/answer', { respuesta: 'Incorrecto', title: 'Quiz' });
+		}
+	})
 };
